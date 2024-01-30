b9d22a836288e32ee32bc76373369f8eabca302d
Sergio Utrera <sergiactive@gmail.com>
2024-01-30 18:23:31 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<h1>Hey</h1>
+
+	<?php wp_footer(); ?>
+</body>
+</html>
